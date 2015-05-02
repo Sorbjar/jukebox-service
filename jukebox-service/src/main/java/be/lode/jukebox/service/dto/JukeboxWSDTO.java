edae36eae655269d0f202a77d12acad9ae944416
Lode Deckers <lode.deckers@gmail.com>
2015-05-02 17:43:42 +0200

    Updates for webservice

---
+++ jukebox-service/src/main/java/be/lode/jukebox/service/dto/JukeboxWSDTO.java
@@ -0,0 +1,24 @@
+package be.lode.jukebox.service.dto;
+
+public class JukeboxWSDTO {
+
+	private String id;
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
