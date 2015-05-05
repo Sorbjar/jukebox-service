10ed46f64888304d2be6089349fd10545ce29f47
Lode Deckers <lode.deckers@gmail.com>
2015-05-05 14:09:15 +0200

    prepare for payment in frontend

--- jukebox-service/src/main/java/be/lode/jukebox/service/mapper/JukeboxModelMapper.java
+++ jukebox-service/src/main/java/be/lode/jukebox/service/mapper/JukeboxModelMapper.java
@@ -5,6 +5,7 @@ import org.modelmapper.ModelMapper;
 import be.lode.jukebox.business.model.Song;
 import be.lode.jukebox.service.dto.SongDTO;
 import be.lode.jukebox.service.mapper.maps.AccountMap;
+import be.lode.jukebox.service.mapper.maps.JukeboxPaymentWSDTOMap;
 import be.lode.jukebox.service.mapper.maps.OAuthApiInfoMap;
 import be.lode.jukebox.service.mapper.maps.OAuthUserMap;
 import be.lode.jukebox.service.mapper.providers.SongProvider;
@@ -21,6 +22,7 @@ public class JukeboxModelMapper extends ModelMapper {
 		this.addMappings(new OAuthApiInfoMap());
 		this.addMappings(new OAuthUserMap());
 		this.addMappings(new AccountMap());
+		this.addMappings(new JukeboxPaymentWSDTOMap());
 	}
 
 	private void createTypeMaps() {
